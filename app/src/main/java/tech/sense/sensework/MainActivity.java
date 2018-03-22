1f89e8fa3b3f77feb44c0555c495f8b798231894
fahrizal <fahrizal.maruf@gmail.com>
2018-03-23 00:50:14 +0800

    adding navigation bar and floating button

--- app/src/main/java/tech/sense/sensework/MainActivity.java
+++ app/src/main/java/tech/sense/sensework/MainActivity.java
@@ -30,20 +30,7 @@ import java.util.ArrayList;
 import devlight.io.library.ntb.NavigationTabBar;
 
 public class MainActivity extends AppCompatActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
 
     @Override
